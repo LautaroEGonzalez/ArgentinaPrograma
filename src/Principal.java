d4fcb36acfb56fc1bb0cb7a475cb1e920e79ede7
LautaroEGonzalez <lautarodt@gmail.com>
2023-02-24 10:56:09 -0300

    Detectar ingresos altos

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,15 @@
 public class Principal {
 	 public static void main(String[] args) {
-			 for (int i = 15; i >= 5; i--) {
-				 if( i % 2 == 0)
-				    System.out.println(i);
-				}
-
+		 double  ingresos = 489.000;
+		 int vehiculos = 2; 
+		 int inmuebles = 1;
+		 boolean embarcacion = false;
+		 	
+		 	if ((ingresos < 489.083) && (vehiculos < 3) && (inmuebles < 3) && (embarcacion == false) ) {
+		 		System.out.println("Usted no pertenece a ingresos altos");
+		 	}
+		 	else {
+		 		System.out.println("Usted pertenece a ingresos altos");	
+		 	}
 	 }
 }
