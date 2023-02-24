9204c66a048c691b811f796822da672ddb65b0b7
LautaroEGonzalez <lautarodt@gmail.com>
2023-02-24 10:20:12 -0300

    Contador con while con opcion

--- src/Principal.java
+++ src/Principal.java
@@ -2,11 +2,22 @@ public class Principal {
 	 public static void main(String[] args) {
 		 int a = 5;
 		 int b = 15;
-	        while (a <= b) {
-	        	if (a % 2 == 0)
-	   		 System.out.println(a);
-	            a++;
-	        }
-	    }
+		 boolean opcion = true;
+		 System.out.println("¿Desea contar numeros pares?");
+				 if (opcion == true) {
+			        while (a <= b) {
+			        	if (a % 2 == 0)
+			   		 System.out.println(a);
+			            a++;
+			        }
+				 	}
+				 	else {
+				        while (a <= b) {
+				        	if (a % 2 != 0)
+				   		 System.out.println(a);
+				            a++;
+				        }
+				 	}
+			    }
 
 }
