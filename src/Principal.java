3c3f7fdfcd98b45098f82a44095c1f3c41112878
LautaroEGonzalez <lautarodt@gmail.com>
2023-02-24 10:10:33 -0300

    Contador con while

---
+++ src/Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+	 public static void main(String[] args) {
+		 int a = 5;
+		 int b = 15;
+	        while (a <= b) {
+	   		 System.out.println(a);
+	            a++;
+	        }
+	    }
+
+}
