a0ef58637b951e31d466d858c0d3514a5e728de2
LautaroEGonzalez <lautarodt@gmail.com>
2023-02-24 10:28:15 -0300

    Contador con for de pares invertidos

--- src/Principal.java
+++ src/Principal.java
@@ -1,23 +1,9 @@
 public class Principal {
 	 public static void main(String[] args) {
-		 int a = 5;
-		 int b = 15;
-		 boolean opcion = true;
-		 System.out.println("¿Desea contar numeros pares?");
-				 if (opcion == true) {
-			        while (a <= b) {
-			        	if (a % 2 == 0)
-			   		 System.out.println(a);
-			            a++;
-			        }
-				 	}
-				 	else {
-				        while (a <= b) {
-				        	if (a % 2 != 0)
-				   		 System.out.println(a);
-				            a++;
-				        }
-				 	}
-			    }
+			 for (int i = 15; i >= 5; i--) {
+				 if( i % 2 == 0)
+				    System.out.println(i);
+				}
 
+	 }
 }
